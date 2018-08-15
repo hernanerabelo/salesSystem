e0ff67b16fb781048a58a414caef9308610984ad
hernane <hernane_rabelo@hotmail.com>
2018-08-15 15:10:32 -0300

    adicionando log4j para aplicacao

--- hgr/src/main/java/br/com/aplication/hgr/Boot.java
+++ hgr/src/main/java/br/com/aplication/hgr/Boot.java
@@ -1,5 +1,8 @@
 package br.com.aplication.hgr;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,16 +13,22 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Boot
 {
+   private static final Logger logger = LogManager.getLogger(Boot.class);
 
    public static void main(String[] args)
    {
       SpringApplication.run(Boot.class, args);
+      logger.debug("Test - Debugging log");
+      logger.info("Test - Info log");
+      logger.warn("Test - Hey, This is a warning!");
+      logger.error("Test - Oops! We have an Error. OK");
+      logger.fatal("Test - Damn! Fatal error. Please fix me.");
    }
+
 
    @GetMapping("/")
    @ResponseBody
-   public String home()
-   {
+   public String home() {
       return "home";
    }
 }
